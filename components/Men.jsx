6f9c2b6b3cefae2a19f0a09e5a26af4de971757e
albertase <albertukaegbu@gmail.com>
2022-11-06 04:12:48 +0100

    Decrease size of product

--- components/Men.jsx
+++ components/Men.jsx
@@ -13,8 +13,8 @@ const Men = ({ product: { image, name, slug, price } }) => {
         <div className="product-card">
           <img 
             src={urlFor(image && image[0])}
-            width={290}
-            height={290}
+            width={250}
+            height={250}
             className="product-image"
           />
           <Badge className='cart' badgeContent={0} color="success">
--- components/Product.jsx
+++ components/Product.jsx
@@ -16,8 +16,8 @@ const Product = ({ product: { image, name, slug, price } }) => {
         <div className="product-card">
           <img 
             src={urlFor(image && image[0])}
-            width={290}
-            height={290}
+            width={250}
+            height={250}
             className="product-image"
           />
         <Badge className='cart' badgeContent={0} color="success">
